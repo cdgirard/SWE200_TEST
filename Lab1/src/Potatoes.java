e5fcf17d71e158ac3f394945db6eb47857401a3a
cdgira <cdgira@cs.ship.edu>
2016-05-18 11:41:37 -0400

    Try 1

---
+++ Lab1/src/Potatoes.java
@@ -0,0 +1,16 @@
+public class Potatoes
+{
+   // Able to view each element of y because it is a primitive 
+    public static void main(String[] args)
+    {
+        int[] y = new int[4];
+        String[] z = new String[4];
+        int x = 3;
+        y[0] = 4;
+        z[0] = "Hi";
+        while (x < 4)
+        {
+           
+        }
+    }
+}
